07f7a5a0107fb4e10ef1fd1fa7290260f0aec424
Timilehin Arigbede <Harriemannie@gmail.com>
2021-08-30 09:35:22 +0100

    Initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Video } from './components/Video';
+import { Menu } from './components/Menu';
 
-import '../assets/stylesheets/application.scss';
-
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
 };
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { src: VIDEOS.fast };
+    this.chooseVideo = this.chooseVideo.bind(this);
+  }
+
+  chooseVideo(newVideo) {
+    this.setState({
+      src: VIDEOS[newVideo]
+    });
+  }
+
+  render() {
+    return (
+      <div className="video">
+        <h1>Video Player</h1>
+        <Menu chooseVideo={this.chooseVideo} />
+        <Video src={this.state.src} />
+      </div>
+    );
+  }
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
